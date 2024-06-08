eb558ed9a410fc178b5294d869e2a796e8206e64
littleduck <littleduck1219@gmail.com>
2024-06-08 23:53:16 +0900

    2-6

--- packages/themes/build.js
+++ packages/themes/build.js
@@ -1,21 +1,33 @@
 import esbuild from 'esbuild';
+import pkg from "./package.json" assert{type: "json"};
+
+const dev = process.argv.includes('--dev');
+const minify = !dev;
+
+const watch = process.argv.includes('--watch');
+
+const external = Object.keys({...pkg.dependencies, ...pkg.peerDependencies});
+
+const baseConfig = {
+    entryPoints: ['src/index.ts'],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    minify,
+    outdir: 'dist',
+    target: "es2019",
+    watch,
+    external
+}
 
 Promise.all([
     esbuild.build({
-        entryPoints: ['src/index.ts'],
-        bundle: true,
-        minify: true,
-        sourcemap: true,
-        outdir: 'dist',
+        ...baseConfig,
         format: 'esm',
     }),
 
     esbuild.build({
-        entryPoints: ['src/index.ts'],
-        bundle: true,
-        minify: true,
-        sourcemap: true,
-        outdir: 'dist',
+        ...baseConfig,
         format: 'cjs',
         outExtension: {
             '.js': '.cjs'
